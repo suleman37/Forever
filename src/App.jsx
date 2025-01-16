5fad7917682cf4c958a63f73eabecce0c5a21810
suleman37 <sulemanshakeel37@gmail.com>
2025-01-16 23:57:16 +0500

    Testing Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <main>
+    <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </main>
+    </>
   );
 };
 
