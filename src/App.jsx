2fa8eb110795cad4fe2cd2bc44179401569a7ae0
suleman37 <sulemanshakeel37@gmail.com>
2025-01-16 23:49:40 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+};
+
+export default App;
