7e6a35b8e88de4add6b34c0c3b6e015dc1d6875a
suleman37 <sulemanshakeel37@gmail.com>
2025-01-17 23:06:49 +0500

    Add Hero Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero';
 
 const Home = () => {
   return (
-    <div>
-       <div className="flex items-center justify-center h-[100vh]">
-         <h1 className="text-3xl font-bold underline text-center">Full Stack Bazar Home Page!!!</h1>
-       </div>
-    </div>
+    <>
+      <Hero/> 
+    </>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <>
+      <div className="flex flex-col sm:flex-row border border-gray-400">
+
+      </div>
+    </>
+  )
+}
+
+export default Hero;
