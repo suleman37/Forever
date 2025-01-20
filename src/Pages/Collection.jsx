12dcad20378ca8aeda91ba4598b1d37718dc5fa1
suleman37 <sulemanshakeel37@gmail.com>
2025-01-20 23:12:11 +0500

    Add Collection Filter Collection

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  return (
-    <div>
-       <div className="flex items-center justify-center h-[100vh]">
-         <h1 className="text-3xl font-bold underline text-center">Full Stack Bazar Collection Page!!!</h1>
-       </div>
-    </div>
-  )
-}
+  const context = useContext(ShopContext);
+  const products = context ? context.products : [];
+  const [ShowFilter, setShowFilter] = useState(false);
 
-export default Collection
+  return (
+    <>
+      <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+        <div className="min-w-60">
+          <p className="my-2 text-xl filter items-center cursor-pointer gap-2">
+            FILTERS
+          </p>
+          <div
+            className={`border border-gray-300 pl-5 py-3 mt-6 ${
+              ShowFilter ? "hidden" : ""
+            }`}
+          >
+            <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+            <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+              <p className="flex gap-2">
+                <input className="w-3" type="checkbox" value={"Men"} /> Men
+              </p>
+              <p className="flex gap-2">
+                <input className="w-3" type="checkbox" value={"Women"} /> Women
+              </p>
+              <p className="flex gap-2">
+                <input className="w-3" type="checkbox" value={"Kids"} /> Kids
+              </p>
+            </div>
+          </div>
+
+          <div
+            className={`border border-gray-300 pl-5 py-3 mt-6 my-5 ${
+              ShowFilter ? "hidden" : ""
+            }`}
+          >
+            <p className="mb-3 text-sm font-medium">TYPES</p>
+            <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+              <p className="flex gap-2">
+                <input className="w-3" type="checkbox" value={"Topwear"} />
+                Topwear
+              </p>
+              <p className="flex gap-2">
+                <input className="w-3" type="checkbox" value={"Bottomwear"} />
+                Bottomwear
+              </p>
+              <p className="flex gap-2">
+                <input className="w-3" type="checkbox" value={"Winterwear"} />
+                Winterwear
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Collection;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-1 text-gray-600">
             <li>+1-212-456-7890</li>
-            <li>contact@forever.com</li>
+            <li>suleman@forever.com</li>
           </ul>
         </div>
       </div>
