619dee025f93dcd7b776b3e71b64979639cde2d7
suleman37 <sulemanshakeel37@gmail.com>
2025-01-20 22:38:02 +0500

    Add NewsLetter , Footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Orders from "./Pages/Orders";
 import PlaceOrder from "./Pages/PlaceOrder";
 import Product from "./Pages/Product";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="/place-order" element={<PlaceOrder />} />
           <Route path="/product/:id" element={<Product />} />
         </Routes>
+        <Footer/>
       </div>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "../assets/frontend_assets/logo.png";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={logo} alt="Forever Logo" className="w-32 mb-5" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae
+            quibusdam deleniti exercitationem.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>contact@forever.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ Suleman Shakeel - All Right Reserved.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
