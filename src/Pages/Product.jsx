9342de10a777839b08d5d000cc9a453256cd30d4
suleman37 <sulemanshakeel37@gmail.com>
2025-01-28 19:24:18 +0500

    Add Cart btn and product decription

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -69,19 +69,50 @@ const Product = () => {
           <div className="flex flex-col gap-4 my-8">
             <p>Select Size</p>
             <div className="flex gap-2">
-              {
-                productData.sizes.map((item, index) => (
-                  <div
-                    className={`border border-gray-300 px-4 py-2 cursor-pointer ${size === item ? "bg-black text-white" : ""}`}
-                    key={index}
-                    onClick={() => setSize(item)}
-                  >
-                    {item}
-                  </div>
-                ))
-              }
+              {productData.sizes.map((item, index) => (
+                <div
+                  className={`border border-gray-300 px-4 py-2 cursor-pointer ${
+                    size === item ? "bg-black text-white" : ""
+                  }`}
+                  key={index}
+                  onClick={() => setSize(item)}
+                >
+                  {item}
+                </div>
+              ))}
+            </div>
+            <button className="border border-black-300 px-4 py-4 cursor-pointer bg-black text-white active:bg-gray-700 w-[500px]">
+              ADD TO CART
+            </button>
+            <hr className="mt-8 sm:4/5" />
+            <div className="text-sm  text-gray-500 mt-5 flex flex-col gap-1">
+              <p>100% Original Product</p>
+              <p>Cash on Delivery is avaliable on this product</p>
+              <p>Easy Return and exchange policy within 7 days</p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="mt-20">
+        <div className="flex">
+          <div className="border px-5 py-3 text-sm">Description</div>
+          <div className="border px-5 py-3 text-sm">Reviews (122)</div>
+        </div>
+        <div className="border px-5 py-3 flex flex-col gap-4 text-sm text-gray-600">
+          <p>
+            Our e-commerce platform is your one-stop shop for all your needs,
+            offering a diverse selection of products ranging from the latest
+            electronics to trendy fashion items. We pride ourselves on providing
+            high-quality products at competitive prices, ensuring that you get
+            the best value for your money.
+          </p>
+          <p>
+            Experience seamless shopping with our user-friendly interface and
+            enjoy exclusive deals and discounts tailored just for you. Whether
+            you're looking for the newest gadgets or the latest fashion trends,
+            our platform is designed to cater to your preferences and make your
+            shopping experience enjoyable and convenient.
+          </p>
         </div>
       </div>
     </div>
