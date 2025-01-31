5beee12511d1e72d17d7e1c3d54d9d6d0e9e3211
suleman37 <sulemanshakeel37@gmail.com>
2025-01-31 10:44:59 +0500

    Related Products Resposiveness

--- src/components/RelatedProduct.jsx
+++ src/components/RelatedProduct.jsx
@@ -22,7 +22,7 @@ const RelatedProduct = ({category , subcategory}) => {
         <div className="text-center text-3xl py-2">
             <Title text1={"RELATED"} text2={"PRODUCT"}/>
         </div>
-        <div className="flex overflow-x-auto gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
             {related.map((item , index) => (
                 <ProductItem key={index} id={item.id} name={item.name} price={item.price} image={item.image}/>
             ))}
